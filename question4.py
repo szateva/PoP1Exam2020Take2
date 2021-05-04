53932f2093d67ccc2c8e999f651d3ff5b7214e4e
Eva Szatmari <szateva@gmail.com>
2021-05-04 15:00:41 +0100

    question5.py answered

--- question4.py
+++ question4.py
@@ -1,10 +1,10 @@
 # 13021326
 
-""" Assume f(x) and g(x) are functions that are dened for numbers x and their return
+""" Assume f(x) and g(x) are functions that are defined for numbers x and their return
 values are also numbers. Implement a higher-order function:
  func_max(f,g)
 that takes as parameters the functions f and g as above. It returns the function h(x)
-that is dened on numbers x and the return value of this function on a given x is equal
+that is defined on numbers x and the return value of this function on a given x is equal
 to the maximum of m and n, where m = f(x) and n = g(x). Importing any libraries
 is NOT allowed.
 Indicative test cases:
@@ -18,3 +18,22 @@ assert h1(3) == 9 #must be True
 h2 = func_max(double, abs)
 #abs is standard Python function computing absolute value of number
 assert h2(-2) == 2 #must be True"""
+
+def func_max(f,g):
+    def h(x):
+        h = max(f(x), g(x))
+        return h
+    return h
+
+
+# Indicative test cases:
+def double(x):
+    return 2*x
+def square(x):
+    return x*x
+h1 = func_max(double, square)
+assert h1(1) == 2 #must be True
+assert h1(3) == 9 #must be True
+h2 = func_max(double, abs)
+#abs is standard Python function computing absolute value of number
+assert h2(-2) == 2 #must be True
--- question5.py
+++ question5.py
@@ -3,17 +3,32 @@
 """ Implement the function
 balance(init_sum, int_rate, tfl, tax_rate, M)
 that calculates the current balance of a savings bank account at the end of M months
-period. The calculation must re
-ect the following assumptions:
- In the beginning of the initial month, the balance is init_sum
- Every month, the monthly interest is calculated as int_rate percent of the bal-
+period. The calculation must reflect the following assumptions:
+ In the beginning of the initial month, the balance is init_sum
+ Every month, the monthly interest is calculated as int_rate percent of the bal-
 ance s available in the beginning of this month
- Every month, the monthly tax is calculated as tax_rate percent of s - tfl, if
+ Every month, the monthly tax is calculated as tax_rate percent of s - tfl, if
 the balance s in the beginning of this month is greater than tfl (tax-free limit).
 Otherwise, the monthly tax is 0.
- At the end of each month, the balance is updated by adding the monthly interest
+ At the end of each month, the balance is updated by adding the monthly interest
 and subtracting the monthly tax.
 Indicative test cases:
 balance(10000, 1, 20000, 1, 2) #must be approximately 10201
 balance(25000, 2, 20000, 1, 2) #must be approximately 25904.5
 balance(19800, 2, 20000, 1, 2) #must be approximately 20597.96 """
+
+def balance(init_sum, int_rate, tfl, tax_rate, M):
+    s = init_sum
+    for m in range(M):
+        interest = s*int_rate/100
+        if s > tfl:
+            tax = (s - tfl) * tax_rate/100
+        else:
+            tax = 0
+        s += interest - tax
+    return s
+
+# Indicative test cases:
+print(balance(10000, 1, 20000, 1, 2)) #must be approximately 10201
+print(balance(25000, 2, 20000, 1, 2)) #must be approximately 25904.5
+print(balance(19800, 2, 20000, 1, 2)) #must be approximately 20597.96
