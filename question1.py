e5c0226cc1f106c27a2117a6e897919d0aaf4452
Eva Szatmari <szateva@gmail.com>
2021-05-04 13:51:19 +0100

    question1.py answered

--- question1.py
+++ question1.py
@@ -1,5 +1,8 @@
 # 13021326
 
+""" The function takes two lists of numbers of equal length and returns a list of the same length
+containing the arithmetic mean of the elements of the original lists index by index. """
+
 """ Consider the following recursive function:
 def fun(L1, L2):
 if L1==[] and L2==[]:
@@ -11,3 +14,14 @@ numbers.
 Find the best English description of the purpose of this function. The use of mathe-
 matical formulas or code in your description should be minimal, this is important in
 marking. Recommended answer length up to 35 words. """
+
+def fun(L1, L2):
+    if L1==[] and L2==[]:
+        return []
+    else:
+        return [(L1[0]+L2[0])/2]+fun(L1[1:], L2[1:])
+
+L1 = [1, 2, 3, 4, 5]
+L2 = [4, 4, 4, 4, 4]
+
+print(fun(L1, L2))
